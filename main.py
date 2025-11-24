e1f5cd1c014f88dcc3e3f75af71414fc7c45e98e
Steven Lau <github@stevenlau.net>
2025-11-24 13:43:24 +0000

    Catapult streamlit app

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+
+
+st.title("Catapult")
+st.write("Results from tests runs")
+st.write("REST endpoint: https://rpqvcjmfyudiehdyx2ykgqzweu0oaxht.lambda-url.eu-west-2.on.aws/")
+st.write("GraphQL endpoint: https://6a5dhgnf5yzokyxy46uzz3sija0kwedl.lambda-url.eu-west-2.on.aws/graphql")
+
+
+result = {
+    "result": ['failure', 'success'],
+    "count": [8, 4],
+}
+
+df = pd.DataFrame(result, index=('result', 'count'))
+df = df.set_index("result")
+
+st.write("---")
+st.table(df)
+st.write("---")
+st.write("Test results")
+st.bar_chart(
+    df,
+    x_label="Status",
+    y_label="Results")
